def27e19bdcbf3bc3dc8a5840541bcbc21d880ac
Vasya2005nezal <vasyapinfo@ya.ru>
2024-05-09 12:26:54 +0300

    Initial commit 2

--- thermoprj.py
+++ thermoprj.py
@@ -1,1 +1,8 @@
-import pyvisa
+from pyvisa import ResourceManager
+
+rm = ResourceManager()
+resources = rm.list_resources()
+
+thermo = rm.open_resource('Температурный контроллер ТС322')
+thermo = rm.open_resource('Источник тока Keysight N5700')
+thermo = rm.open_resource('Вольтметр 34401a')
