92c6b9e882f6b315a078c2d13003a7048a70cc43
ivmon <ivmartynovb@gmal.com>
2024-05-09 12:36:29 +0300

    g4tg

--- thermoprj.py
+++ thermoprj.py
@@ -5,4 +5,5 @@ resources = rm.list_resources()
 
 thermo = rm.open_resource('Температурный контроллер ТС322')
 current = rm.open_resource('Источник тока Keysight N5700')
-voltage = rm.open_resource('Вольтметр 34401a')
+# voltage = rm.open_resource('Вольтметр 34401a')
+
