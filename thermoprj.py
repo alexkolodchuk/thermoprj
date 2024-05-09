211ca154f65b79fc2d779038d30493e9ef36cb5f
Vasya2005nezal <vasyapinfo@ya.ru>
2024-05-09 14:03:32 +0300

    Version 1.1: Change arrays to numpy arrays

--- thermoprj.py
+++ thermoprj.py
@@ -2,34 +2,35 @@ from pyvisa import ResourceManager
 from time import time
 import numpy as np
 import matplotlib.pyplot as plt
-
+from pyvisa.resources import Resource
 
 rm = ResourceManager()
 resources = rm.list_resources()
 
-thermo = rm.open_resource('Температурный контроллер ТС322')
-current = rm.open_resource('Источник тока Keysight N5700')
-voltage = rm.open_resource('Вольтметр 34401a')
+thermo: Resource = rm.open_resource('Температурный контроллер ТС322')
+current: Resource = rm.open_resource('Источник тока Keysight N5700')
+voltage: Resource = rm.open_resource('Вольтметр 34401a')
 
 current_now = 1.0
 current.write(f"SOURce:CURRent:LEVel:IMMediate:AMPLitude {current_now}")
 
-temps = np.linspace(300.000, 350.000, 25)
+temps = np.linspace(300, 350, 25)
 eps = 0.005
-volt_sp = []
-temp_sp = []
-resistance_sp = []
+volt_sp = np.array([])
+temp_sp = np.array([])
+resistance_sp = np.array([])
 graph = plt.plot(temp_sp, resistance_sp)
+
 for i in temps:
     temp_now = thermo.query('MEASure[n]:TEMPerature?')
-    thermo.write(f'PID[n]:TEMPerature:TARGet {i}')
+    thermo.write(f'PID[n]:TEMPerature:TARGet {i:3.3f}')
     while abs(temp_now - i) >= eps:
-        temp_now = (thermo.query('MEASure[n]:TEMPerature?'))
+        temp_now = thermo.query('MEASure[n]:TEMPerature?')
     volts = voltage.query('read?')
-    temp_sp.append(temp_now)
-    volt_sp.append(volts)
+    temp_sp = np.append(temp_sp, temp_now)
+    volt_sp = np.append(volt_sp, volts)
     resistance = volts / current_now
-    resistance_sp.append(resistance)
+    resistance_sp = np.append(resistance_sp, resistance)
     graph.remove()
     graph = plt.plot(temp_sp, resistance_sp)
 
