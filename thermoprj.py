971758ad3a841c38d583abf641a467ce3d97f77c
Vasya2005nezal <vasyapinfo@ya.ru>
2024-05-09 12:27:05 +0300

    Initial commit 2

--- thermoprj.py
+++ thermoprj.py
@@ -4,5 +4,5 @@ rm = ResourceManager()
 resources = rm.list_resources()
 
 thermo = rm.open_resource('Температурный контроллер ТС322')
-thermo = rm.open_resource('Источник тока Keysight N5700')
-thermo = rm.open_resource('Вольтметр 34401a')
+current = rm.open_resource('Источник тока Keysight N5700')
+voltage = rm.open_resource('Вольтметр 34401a')
