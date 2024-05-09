07b3691e9c96b9b14bb308cca2f0f94246af2506
ivmon <ivmartynovb@gmal.com>
2024-05-09 13:43:17 +0300

    Versia 1

--- thermoprj.py
+++ thermoprj.py
@@ -1,4 +1,8 @@
 from pyvisa import ResourceManager
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 rm = ResourceManager()
 resources = rm.list_resources()
@@ -6,4 +10,27 @@ resources = rm.list_resources()
 thermo = rm.open_resource('Температурный контроллер ТС322')
 current = rm.open_resource('Источник тока Keysight N5700')
 voltage = rm.open_resource('Вольтметр 34401a')
+
+current_now = 1.0
+current.write(f"SOURce:CURRent:LEVel:IMMediate:AMPLitude {current_now}")
+
+temps = np.linspace(300.000, 350.000, 25)
+eps = 0.005
+volt_sp = []
+temp_sp = []
+resistance_sp = []
+graph = plt.plot(temp_sp, resistance_sp)
+for i in temps:
+    temp_now = thermo.query('MEASure[n]:TEMPerature?')
+    thermo.write(f'PID[n]:TEMPerature:TARGet {i}')
+    while abs(temp_now - i) >= eps:
+        temp_now = (thermo.query('MEASure[n]:TEMPerature?'))
+    volts = voltage.query('read?')
+    temp_sp.append(temp_now)
+    volt_sp.append(volts)
+    resistance = volts / current_now
+    resistance_sp.append(resistance)
+    graph.remove()
+    graph = plt.plot(temp_sp, resistance_sp)
+
 
